953c8999cb301397a3dd997ad56f9e711483ac3a
3D ALGO LAB <threealgo@outlook.com>
2024-07-01 08:31:07 +0900

    gemma params analyzed

--- jora/__init__.py
+++ jora/__init__.py
@@ -4,8 +4,8 @@ from .lib.gemma.common import ParagemmaConfig
 from .lib.gemma.common import generate_alpaca_dataset as generate_alpaca_dataset_gemma
 from .lib.gemma.common import train_lora as train_lora_gemma
 
-from .lib.paligemma.common import ParaPaliGemmaConfig
-from .lib.paligemma.common import train_lora as train_lora_paligemma
+# from .lib.paligemma.common import ParaPaliGemmaConfig
+# from .lib.paligemma.common import train_lora as train_lora_paligemma
 
 # allow importing this module directly
 __all__ = [
@@ -16,6 +16,6 @@ __all__ = [
     "ParagemmaConfig",
     "generate_alpaca_dataset_gemma",
     "train_lora_gemma",
-    "ParaPaliGemmaConfig",  # newly added for PaliGemma
-    "train_lora_paligemma",  # newly added for PaliGemma
+    # "ParaPaliGemmaConfig",  # newly added for PaliGemma
+    # "train_lora_paligemma",  # newly added for PaliGemma
 ]
